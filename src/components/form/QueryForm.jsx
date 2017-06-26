c2d947c1c3ffbb4a6662e7aa950c1ecd5d990389
unknown <>
2017-06-26 11:20:35 +0800

    注解

--- src/components/form/QueryForm.jsx
+++ src/components/form/QueryForm.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import { Input, Select, Button, message, Form, DatePicker } from 'antd';
 
+/**
+ * QueryForms定义了两个props，分别是enterKey和aotu_focus。
+ * enterKey为函数，即当按下回车键时执行该函数，可选择不设置。默认不设置
+ * aotu_focus为页面加载后自动聚焦，布尔值。true为开启。默认false
+ * 
+ * input: {type: 'input'(必填), text: 输入框名称(必填), parms: ant-design中input接口,
+ * 				 queryName,queryValue,queryReg为query按钮的自动拼接字段---queryName:{[queryReg]:queryValue} }
+ * 
+ * select: {type: 'select'(必填), text: 复选框名称(必填), parms: ant-design中select接口,
+ * 					options: select下的option，为数组，每个数组下有opValue和opText(必填),
+ * 				 queryName,queryValue,queryReg为query按钮的自动拼接字段---queryName:{[queryReg]:queryValue} }
+ * 
+ * dateTime: {type: 'dateTime'(必填), text: 日期选择名称(必填), parms: ant-design中日期控件接口,
+ * 					  queryName,startQueryValue,endQueryValue为query按钮的自动拼接字段
+ * 						---queryName:{'$gte':startQueryValue,'$lte':endQueryValue}}
+ * 
+ * button: {type: 'select'(必填), text: 按钮名称(必填), parms: ant-design中button接口,
+ * 					queryName,queryValue,queryReg为query按钮的自动拼接字段---queryName:{[queryReg]:queryValue}}
+ * 
+ * query按钮: 自动拼接选项字段
+ *         {clickFunction:点击后执行函数，在拼接完对象后执行，可将拼接完的对象当参数传入,详情看案例，
+ * 					parms:ant-design中button接口,}
+ */
+
 const Option = Select.Option
 const FormItem = Form.Item
 const formItemLayout = {
