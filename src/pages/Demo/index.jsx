dacf1022a950fcfcfb27f7ee1c111c14fa907cef
MeetTheBest <1278298417@qq.com>
2019-07-04 15:24:22 +0800

    refactor: 【【通用】响应式布局整体方案】
    https://www.tapd.cn/22105901/prong/stories/view/1122105901001001531

--- src/pages/Demo/index.jsx
+++ src/pages/Demo/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Header from './page/Header';
 import QueryBlock from './page/QueryBlock';
 
 export default () => {
   return (
     <div>
+      <Header />
       <QueryBlock />
     </div>
   );
