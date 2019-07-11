f647ece762f2124ab998f44276763f6e19e69ff2
kang12345 <18859727527@163.com>
2019-07-11 17:25:02 +0800

    fix: 【【emweb】刷新页面后，左侧导航栏无法记住之前的收缩/展开状态】https://www.tapd.cn/22105901/bugtrace/bugs/view?bug_id=1122105901001000648

--- src/layout/SideMenu/index.jsx
+++ src/layout/SideMenu/index.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { SideMenu } from '@1msoft/kant-ui';
-
+import { getCookie, setCookie } from '@utils/helper.js';
 import './index.less';
 
 export default (props) => {
+  const [sideMenuStatus, setSideMenuStatus] = useState(
+    getCookie('menu_status')
+  );
   return (
     <SideMenu
       useCollapsed={true}
       selectedKeys={props.selectKeys}
       openKeys={props.openKeys}
       dataSource={props.dataSource}
+      isCollapsed={{ isOpen: JSON.parse(sideMenuStatus) }}
       inlineOpenStyle="normal"
       onJumpway={(url) => { props.history.push(url); }}
       siderProps={{ theme: 'light' }}
+      onSiderCollapse={(val) => { setCookie('menu_status', val); }}
     />
   );
 };
